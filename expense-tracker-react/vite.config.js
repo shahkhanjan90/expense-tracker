126108c2dee808149d04ef5ddf3cd93ea465579a
Khanjan Shah <shah.khanjan90@gmail.com>
2026-04-12 23:56:56 +0530

    publish

--- expense-tracker-react/vite.config.js
+++ expense-tracker-react/vite.config.js
@@ -3,9 +3,11 @@ import react from '@vitejs/plugin-react'
 
 const SCRIPT_BASE_URL =
   'https://script.google.com/macros/s/AKfycbwEQbN1F1h5kFVlIVmzuOj0ZR2G2D0pnybDAYWqB10JspEvgCJGlJ8yNiqlfiY0_fE/exec'
+const PAGES_BASE_PATH = '/expense-tracker-react/'
 
 // https://vite.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
+  base: mode === 'production' ? PAGES_BASE_PATH : '/',
   plugins: [react()],
   server: {
     proxy: {
@@ -16,4 +18,4 @@ export default defineConfig({
       },
     },
   },
-})
+}))
